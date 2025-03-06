5176f11436413f328def43b8d582aeb11ec356e1
valenmazzoni <mazzonivalentina79@gmail.com>
2025-03-06 18:31:20 -0300

    Add files via upload

---
+++ rodmap.js
@@ -0,0 +1,19 @@
+/*RODMAP-JAVASCRIP-MOREDEV*/
+//EJERCICIO-00//
+
+//URL>>https://www.javascript.com//
+
+//Comentarios en una sola linea//
+
+/*comentario en
+multiple linea*/
+
+let variable = "variable1"
+
+const constante = true
+
+let numeros = 1234
+let string = "JuanCarlos"
+let booleans = false
+
+console.log("Hola, JavaScript!");
